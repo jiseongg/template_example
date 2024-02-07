2ea0d944e3ebce20b9e25030e36e2409f617adc8
jiseongg <jiseongg@users.noreply.github.com>
2024-02-07 07:14:46 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jiseongg"
+original_name="template_example"
+original_urlname="template_example"
+original_description="Awesome template_example created by jiseongg"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv template_example $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ template_example/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for template_example."""
+
+from template_example.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
